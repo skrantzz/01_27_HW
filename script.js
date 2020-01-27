4bca4c9c7affd4f7e48bbb272f94f42adb9eefee
Sydney Krantz <syd107@gmail.com>
2020-01-26 21:53:00 -0800

    second commit with buttons and timer semi working

---
+++ script.js
@@ -0,0 +1,97 @@
+var highscores = $("#highscores");
+var questionContainer = $("#question-container");
+var startButton = $("#start-btn");
+var nextButton = $("#next-btn");
+var submitButton = $("#submit-btn");
+var timer = $("#countdown");
+var timeGauge = $("#timeGauge");
+var progress = $("#progress");
+var scoreContainer = $("#scoreContainer");
+var timeLeft = 15;
+
+var questions = [
+  {
+    question: "What is Javascript?",
+    answers: [
+      "A client-side scripting language",
+      "A server-side scripting language that is understood by web browsers",
+      "A & B"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Which one is NOT a JavaScript data type?",
+    answers: ["Number", "Sentence", "Undefined"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which company developed JavaScript",
+    answers: ["Netgear", "Netscape", "Napster"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is 'this' keyword?",
+    answers: [
+      "A keyword referring to the object from where it was called",
+      "The opposite of 'that'",
+      "A keyword referring to the function from where it was called "
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which symbol is used to comment something?",
+    answers: ["//", "/* */", "A & B"],
+    correctAnswer: 2
+  },
+  {
+    question: "Which is NOT type of pop up box in JavaScript?",
+    answers: ["Alert", "Prompt", "Message"],
+    correctAnswer: 2
+  },
+  {
+    question: "What are JavaScript Cookies?",
+    answers: [
+      "Chocolate Chip",
+      "The small test files stored in a computer which gets created when the user visits a website to store information they need",
+      "Oreos"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: "Why is Sydney so great?",
+    answers: ["She's cool", "She's pretty", "A & B"],
+    correctAnswer: 2
+  }
+];
+
+//hide start page when start button is clicked
+$("#start-btn").click(function() {
+  $("#opener").hide();
+  renderQuestion(questions[0]);
+  //timer
+  var timerInterval = setInterval(function() {
+    timeLeft--;
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+    }
+    timer.text("Time Left: " + timeLeft);
+    // console.log(timeLeft);
+  }, 1000);
+});
+
+function renderQuestion(questionArgument) {
+  var questionElement = document.createElement("div");
+  questionElement.innerHTML = questionArgument.question;
+  questionContainer.append(questionElement);
+
+  for (var i = 0; i < questionArgument.answers.length; i++) {
+    var createButton = document.createElement("button");
+    createButton.textContent = questionArgument.answers[i];
+    questionContainer.append(createButton);
+
+    createButton.onclick = function() {};
+    if (i === questionArgument.correctAnswer) {
+      //   // do some correct answer stuff like go to next question or finish quiz or add to score
+    }
+  }
+}
